c0062b35a7fff4207620160970bc5a2fd667ae0a
donkere-vader <donkere.v@gmail.com>
2022-01-30 18:31:20 +0100

    Added argument parser

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 //! This AI was written for my Profielwerkstuk (dutch highschool thesis)
 
 use uci::{ uci };
+use argparse::{ArgumentParser, Store};
 
 mod consts;
 mod game;
@@ -19,5 +20,23 @@ mod uci;
 mod tests;
 
 fn main() {
-    uci();
+    let mut mode = String::new();
+
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+
+        ap.set_description("Run chess AI.");
+
+        ap.refer(&mut mode)
+            .add_option(&["--mode"], Store,
+            "Mode to run chess AI in. Choose from: [UCI]");
+        ap.parse_args_or_exit();
+
+    }
+
+    if mode == "uci".to_string() {
+        uci();
+    } else {
+        panic!("'{}' is not a valid mode.", mode);
+    }
 }
